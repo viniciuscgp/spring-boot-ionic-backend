1f7058ed281b98d1e927e9a1ce8cf49daed04760
Vinicius Cesar <vinians2006@yahoo.com.br>
2023-01-16 22:23:34 -0300

    Criando repository e service para categorias

--- src/main/java/br/app/narede/cursomc/resources/CategoriaResource.java
+++ src/main/java/br/app/narede/cursomc/resources/CategoriaResource.java
@@ -1,28 +1,27 @@
 package br.app.narede.cursomc.resources;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.app.narede.cursomc.domain.Categoria;
+import br.app.narede.cursomc.services.CategoriaService;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 	
-	@GetMapping
-	public List<Categoria> listar() {
-		Categoria cat1 = new Categoria(1, "Informática");
-		Categoria cat2 = new Categoria(2, "Escritório");
-		
-		List<Categoria> lista = new ArrayList<>();
-		lista.add(cat1);
-		lista.add(cat2);
-		
-		return lista;
+	@Autowired
+	private CategoriaService service;
+	
+	@GetMapping(value="/{id}")
+	public ResponseEntity<?> find(@PathVariable Integer id) {
+		Categoria obj = service.buscar(id);
+
+		return ResponseEntity.ok().body(obj);
 	}
 }
 
