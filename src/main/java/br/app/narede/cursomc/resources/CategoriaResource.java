177a204ae3ce78543fc9ebaa320fed4cc27f1a49
Vinicius Cesar <vinians2006@yahoo.com.br>
2023-01-28 19:54:47 -0300

    Atualizando Categoria com PUT, outros pequenos ajustes

--- src/main/java/br/app/narede/cursomc/resources/CategoriaResource.java
+++ src/main/java/br/app/narede/cursomc/resources/CategoriaResource.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -24,8 +25,8 @@ public class CategoriaResource {
 	private CategoriaService service;
 	
 	@GetMapping(value="/{id}")
-	public ResponseEntity<?> find(@PathVariable Integer id) {
-		Categoria obj = service.buscar(id);
+	public ResponseEntity<Categoria> find(@PathVariable Integer id) {
+		Categoria obj = service.find(id);
 
 		return ResponseEntity.ok().body(obj);
 	}
@@ -39,6 +40,13 @@ public class CategoriaResource {
 		return ResponseEntity.created(uri).build();
 	} 
 	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<Void> update(@RequestBody Categoria obj, @PathVariable Integer id) {
+		obj.setId(id);
+		obj = service.update(obj);
+
+		return ResponseEntity.noContent().build();
+	} 
 	
 }
 
