13c507b5b7b4ca956f91d4de76a28b8ed365fb74
Vinicius Cesar <vinians2006@yahoo.com.br>
2023-01-28 19:41:57 -0300

    Inserindo uma nova Categoria com POST

--- src/main/java/br/app/narede/cursomc/resources/CategoriaResource.java
+++ src/main/java/br/app/narede/cursomc/resources/CategoriaResource.java
@@ -1,11 +1,17 @@
 package br.app.narede.cursomc.resources;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.app.narede.cursomc.domain.Categoria;
 import br.app.narede.cursomc.services.CategoriaService;
@@ -23,5 +29,16 @@ public class CategoriaResource {
 
 		return ResponseEntity.ok().body(obj);
 	}
+	
+	@PostMapping
+	public ResponseEntity<Void> insert(@RequestBody Categoria obj) {
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(obj.getId()).toUri();
+		
+		return ResponseEntity.created(uri).build();
+	} 
+	
+	
 }
 
