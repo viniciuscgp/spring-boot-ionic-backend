85eaa6c22acab58f92bfc09c6cdec6a566e67f10
Vinicius Cesar <vinians2006@yahoo.com.br>
2023-01-29 00:39:36 -0300

    Deletando uma Categoria com DELETE

--- src/main/java/br/app/narede/cursomc/resources/CategoriaResource.java
+++ src/main/java/br/app/narede/cursomc/resources/CategoriaResource.java
@@ -4,13 +4,13 @@ import java.net.URI;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -47,6 +47,13 @@ public class CategoriaResource {
 
 		return ResponseEntity.noContent().build();
 	} 
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Integer id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	} 
+
 	
 }
 
--- src/main/java/br/app/narede/cursomc/services/CategoriaService.java
+++ src/main/java/br/app/narede/cursomc/services/CategoriaService.java
@@ -3,10 +3,12 @@ package br.app.narede.cursomc.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import br.app.narede.cursomc.domain.Categoria;
 import br.app.narede.cursomc.repositories.CategoriaRepository;
+import br.app.narede.cursomc.services.exceptions.DataIntegrityException;
 import br.app.narede.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -31,6 +33,16 @@ public class CategoriaService {
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		}catch(DataIntegrityViolationException e) {
+			throw new DataIntegrityException(
+					"Não é possivel excluir uma categoria que possui produtos!");			
+		}
 	}
 	
 }
