f7b8829bb4a13ba4c9cf94a47135af1c43e55e77
Vinicius Cesar <vinians2006@yahoo.com.br>
2023-02-04 16:47:16 -0300

    Validação customizada: Email não repetido na inserção do cliente

--- src/main/java/br/app/narede/cursomc/repositories/ClienteRepository.java
+++ src/main/java/br/app/narede/cursomc/repositories/ClienteRepository.java
@@ -1,9 +1,11 @@
 package br.app.narede.cursomc.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import br.app.narede.cursomc.domain.Cliente;
 
 public interface ClienteRepository extends JpaRepository<Cliente, Integer>{
-	
+	@Transactional(readOnly=true)
+	Cliente findByEmail(String email);
 }
--- src/main/java/br/app/narede/cursomc/services/validation/ClienteInsertValidator.java
+++ src/main/java/br/app/narede/cursomc/services/validation/ClienteInsertValidator.java
@@ -6,13 +6,20 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import br.app.narede.cursomc.domain.Cliente;
 import br.app.narede.cursomc.domain.enums.TipoCliente;
 import br.app.narede.cursomc.dto.ClienteNewDTO;
+import br.app.narede.cursomc.repositories.ClienteRepository;
 import br.app.narede.cursomc.resources.exceptions.FieldMessage;
 import br.app.narede.cursomc.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
-
+	
+	@Autowired
+	private ClienteRepository repo;
+	
 	@Override
 	public  void initialize(ClienteInsert ann) {
 		
@@ -35,6 +42,12 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 			}
 		}
 			
+		Cliente aux = repo.findByEmail(objDto.getEmail());
+		
+		if (aux != null) {
+			list.add(new FieldMessage("email", "Email já existente"));
+		}
+		
 		
 		for (FieldMessage e: list) {
 			context.disableDefaultConstraintViolation();
