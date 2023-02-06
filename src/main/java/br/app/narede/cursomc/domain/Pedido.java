d61107a94930113dbf1090341c44d2db2a268fa5
Vinicius Cesar <vinians2006@yahoo.com.br>
2023-02-05 22:46:52 -0300

    Subtotal e total do Pedido

--- src/main/java/br/app/narede/cursomc/domain/Pedido.java
+++ src/main/java/br/app/narede/cursomc/domain/Pedido.java
@@ -54,6 +54,16 @@ public class Pedido implements Serializable{
 		this.enderecoDeEntrega = enderecoDeEntrega;
 	}
 
+	public double getValorTotal()
+	{
+		double soma = 0.0;
+		for (ItemPedido item:itens) {
+			soma += item.getSubTotal();
+		}
+		return soma;
+		
+	}
+	
 	public Integer getId() {
 		return id;
 	}
